ecf080814e7868a2e3a0255aa67f69e9c621ac5a
SergeiK1 <sergeikdr1@gmail.com>
2023-08-19 10:32:36 -0500

    asda

--- src/pages/Poems.js
+++ src/pages/Poems.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import '../css/Poems.css'; // Import CSS file for component styling
 
+
+const poemsData = [
+    {
+      title: 'Poem 1',
+      image: 'poem1.jpg',
+      text: 'Lorem ipsum dolor sit amet...',
+    },
+    // Add more poem data here
+  ];
+
+
 export default function Poems() {
   return (
-    <div>Poems</div>
+    <div className="masonry-layout">
+    {poemsData.map((poem, index) => (
+      <div className="masonry-item" key={index}>
+        <img src={poem.image} alt={poem.title} />
+        <h3>{poem.title}</h3>
+        <p>{poem.text}</p>
+      </div>
+    ))}
+  </div>
   )
 }
